396dbb64b312f5c80346de8f249971f6f3245d0c
shunak <shuna966@gmail.com>
2023-03-17 16:21:16 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("引数の個数が正しくありません");
+        process::exit(1);
+    }
+
+    let num: i32 = args[1].parse().unwrap();
+
+    println!("{}", num);
+}
+
