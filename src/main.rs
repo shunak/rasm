812be19117bf93b1023b672f406b524a1b8294db
shunak <shuna966@gmail.com>
2023-04-03 15:45:46 +0900

    Modified: Not using unsafe

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,7 @@ fn main() {
         std::process::exit(1);
     }
 
-    let mut p = args[1].as_bytes().as_ptr();
-    let end = unsafe { p.add(args[1].len()) };
+    let mut p = args[1].chars().peekable();
 
     println!(".intel_syntax noprefix");
     println!(".globl main");
@@ -17,33 +16,35 @@ fn main() {
     let mut num = parse_int(&mut p);
     println!("  mov rax, {}", num);
 
-    while p < end {
-        if unsafe { *p } == b'+' {
-            p = unsafe { p.add(1) };
-            num = parse_int(&mut p);
-            println!("  add rax, {}", num);
-            continue;
+    while let Some(op) = p.next() {
+        match op {
+            '+' => {
+                let num = parse_int(&mut p);
+                println!("  add rax, {}", num);
+            }
+            '-' => {
+                let num = parse_int(&mut p);
+                println!("  sub rax, {}", num);
+            }
+            _ => {
+                eprintln!("予期しない文字です: '{}'", op);
+                std::process::exit(1);
+            }
         }
-
-        if unsafe { *p } == b'-' {
-            p = unsafe { p.add(1) };
-            num = parse_int(&mut p);
-            println!("  sub rax, {}", num);
-            continue;
-        }
-
-        eprintln!("予期しない文字です: '{}'", unsafe { *p } as char);
-        std::process::exit(1);
     }
 
     println!("  ret");
 }
 
-fn parse_int(p: &mut *const u8) -> i64 {
+fn parse_int(p: &mut std::iter::Peekable<std::str::Chars>) -> i64 {
     let mut num = 0;
-    while unsafe { (**p as char).is_digit(10) } {
-        num = num * 10 + (unsafe { **p } - b'0') as i64;
-        *p = unsafe { p.add(1) };
+    while let Some(c) = p.peek().cloned() {
+        if c.is_digit(10) {
+            num = num * 10 + c.to_digit(10).unwrap() as i64;
+            p.next();
+        } else {
+            break;
+        }
     }
     num
 }
